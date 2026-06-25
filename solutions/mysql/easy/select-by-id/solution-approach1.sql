94fa22765af7e0261bbf5a2b619b104326ef91bf
Jeet Kumar Singh Singh <jeetkumarsingh07india@gmail.com>
2026-06-25 10:09:00 +0530

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-06-25, 10:08 a.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * from CITY 
+where ID = 1661;
