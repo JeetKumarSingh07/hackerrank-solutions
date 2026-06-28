540c5ecc0c64bff5d27a8953413cdb3bc45aec92
Jeet Kumar Singh Singh <jeetkumarsingh07india@gmail.com>
2026-06-28 20:24:41 +0530

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-06-28, 08:24 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT NAME from CITY 
+where  COUNTRYCODE ='JPN';
