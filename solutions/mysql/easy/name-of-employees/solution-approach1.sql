f9df38126ea21fb46d3fc4826dfd632dfe6ec04b
Jeet Kumar Singh <jeetkumarsingh07india@gmail.com>
2026-07-01 14:10:53 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-07-01, 02:10 p.m.
+-- ──────────────────────────────────────────────────
+
+/*
+Enter your query here.
+*/
+select name from Employee order by name;
