5418381d1103e1e35c486a5d614c09742526f8de
Jeet Kumar Singh <jeetkumarsingh07india@gmail.com>
2026-08-11 11:49:42 +0530

    ✅ Extra Long Factorials [python3] — approach1

---
+++ solutions/python3/medium/extra-long-factorials/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/extra-long-factorials/problem?isFullScreen=true
+# Problem     Extra Long Factorials
+# Difficulty  Medium
+# Subdomain   Implementation
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-08-11, 11:49 a.m.
+# ──────────────────────────────────────────────────
+
+import math
+
+n = int(input().strip())
+print(math.factorial(n))
